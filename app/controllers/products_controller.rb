a8dd86822b61e9551e6a59ee0c4ff47daa28c0a0
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-05-04 11:28:09 -0700

    style product detail card and incorporate form for adding to cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+    @order_item = current_order.order_items.new
     respond_to do |format|
       format.html { redirect_to products_path }
       format.js
