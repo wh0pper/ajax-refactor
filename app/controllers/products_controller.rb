3a669a9893113f84211c5c0e7f22019f0132ce78
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-05-04 10:12:29 -0700

    use ajax to render product detail partial when product name clicked from index page, currently just as template at top of page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,14 @@ class ProductsController < ApplicationController
     @order_item = current_order.order_items.new
   end
 
+  def show
+    @product = Product.find(params[:id])
+    respond_to do |format|
+      format.html { redirect_to products_path }
+      format.js
+    end
+  end
+
   def new
     @product = Product.new
   end
