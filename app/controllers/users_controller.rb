7c2fa61f61f12cf2e65a7693ae506a211ec2cba6
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-05-04 14:56:12 -0700

    add flash messages to sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,11 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
+      flash[:notice] = "Welcome! Your account was created successfully."
       session[:user_id] = user.id
       redirect_to '/'
     else
+      flash[:alert] = "Oops! Something went wrong. Please try again"
       redirect_to '/sign_up'
     end
   end
