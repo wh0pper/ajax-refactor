aa18c57e6983265a504c31fa55b63eb86fc45b7b
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-05-04 16:52:56 -0700

    add uniqueness validations where appropriate

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
   has_many :order_items, dependent: :destroy
+
+  validates :name, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :orders, dependent: :destroy
 
+  validates :email, uniqueness: true
+
   def previous_orders
     self.orders.where(status: 2).includes(order_items: :product)
   end
